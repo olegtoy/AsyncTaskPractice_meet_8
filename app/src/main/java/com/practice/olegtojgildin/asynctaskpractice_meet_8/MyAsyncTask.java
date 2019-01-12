f783070fd635e7dd90da35eeaf37f5c27864d516
Oleg Toygildin <toyoa@ya.ru>
2019-01-12 22:45:52 +0300

    Add AsyncTask

--- app/src/main/java/com/practice/olegtojgildin/asynctaskpractice_meet_8/MyAsyncTask.java
+++ app/src/main/java/com/practice/olegtojgildin/asynctaskpractice_meet_8/MyAsyncTask.java
@@ -1,24 +1,38 @@
 package com.practice.olegtojgildin.asynctaskpractice_meet_8;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by olegtojgildin on 12/01/2019.
  */
 
-public class MyAsyncTask extends AsyncTask<Void,Void,Integer> {
+public class MyAsyncTask extends AsyncTask<Void,Void,Void> {
+    public static final int MESSAGE_ID=1;
+    private Random rnd;
+    private Handler mHandler;
+
+    public MyAsyncTask(Handler handler){
+        rnd=new Random();
+        mHandler=handler;
+    }
     @Override
-    protected Integer doInBackground(Void... voids) {
-        Random rnd=new Random();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    protected Void doInBackground(Void... voids) {
+        while (!isCancelled()) {
+            try {
+               Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message = mHandler.obtainMessage(MESSAGE_ID, rnd.nextInt(100));
+            message.sendToTarget();
         }
-        return rnd.nextInt();
+        return null;g
     }
 
 
--- app/src/main/java/com/practice/olegtojgildin/asynctaskpractice_meet_8/SecondFragment.java
+++ app/src/main/java/com/practice/olegtojgildin/asynctaskpractice_meet_8/SecondFragment.java
@@ -1,6 +1,9 @@
 package com.practice.olegtojgildin.asynctaskpractice_meet_8;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,39 +19,49 @@ import java.util.concurrent.ExecutionException;
 
 public class SecondFragment extends Fragment {
     TextView result;
+    MyAsyncTask myAsyncTask;
+
     public static SecondFragment newInstance() {
-        
         Bundle args = new Bundle();
         SecondFragment fragment = new SecondFragment();
         fragment.setArguments(args);
         return fragment;
     }
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MyAsyncTask.MESSAGE_ID:
+                    result.setText(Integer.toString((Integer) msg.obj));
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    };
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.second_fragment,container,false);
+        View view = inflater.inflate(R.layout.second_fragment, container, false);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        result=view.findViewById(R.id.number);
-
-
+        result = view.findViewById(R.id.number);
     }
-
 
     @Override
     public void onResume() {
         super.onResume();
-        try {
-            for (int i=0;i<10;i++)
-            result.setText(Integer.toString(new MyAsyncTask().execute().get()));
+        myAsyncTask=new MyAsyncTask(mHandler);
+        myAsyncTask.execute();
+    }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void onPause() {
+        super.onPause();
+        myAsyncTask.cancel(true);
     }
 }
